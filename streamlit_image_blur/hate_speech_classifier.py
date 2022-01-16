8cf50759d41f5d16c4c62c08077107138578191d
Mateusz <m.borowski128@gmail.com>
2022-01-16 05:47:16 +0100

    Add move streamlit funcionalities

--- streamlit_image_blur/hate_speech_classifier.py
+++ streamlit_image_blur/hate_speech_classifier.py
@@ -1,10 +1,12 @@
 from transformers import AutoTokenizer, T5ForConditionalGeneration
+import streamlit as st
 
 ckpt = 'Narrativa/byt5-base-tweet-hate-detection'
 
 tokenizer = AutoTokenizer.from_pretrained(ckpt)
 model = T5ForConditionalGeneration.from_pretrained(ckpt).to("cpu")
 
+@st.cache
 def classify(tweet):
 
     inputs = tokenizer([tweet], padding='max_length', truncation=True, max_length=512, return_tensors='pt')
--- streamlit_image_blur/highlight_emotions.py
+++ streamlit_image_blur/highlight_emotions.py
@@ -1,31 +1,41 @@
 import cv2
 import pytesseract
-from hate_speech_classifier import classify
+import streamlit as st
 
-def blur_hate_speech(img):
+from emotion_classifier import classify
+
+colors = {'anger': (255, 0, 0),
+          'disgust': (100, 255, 255),
+          'fear': (100, 255, 0),
+          'joy': (0, 255, 0),
+          'neutral': (255, 255, 255),
+          'sadness': (0, 0, 255),
+          'surprise': (100, 100, 100)
+        }
+
+@st.cache
+def highlight_emotions(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
     rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
     dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
                                                     cv2.CHAIN_APPROX_NONE)
+
+    rgba = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
         
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
         
         cropped = img[y:y + h, x:x + w]
-        is_hate_speech = False
         text = pytesseract.image_to_string(cropped)
         text = text.replace('\n', ' ')
         print("text",text)
-        result = classify(text)
-        print("RESULT",result)
-        if result == "hate-speech":
-                    is_hate_speech = True
+        result = classify(text)[0]['label']
+        print("RESULT", result)
                 
-        if is_hate_speech:
-            roi = img[y:y+h,  x:x+w]
-            roi = cv2.GaussianBlur(roi, (23, 23), 30)
-            img[y:y+roi.shape[0], x:x+roi.shape[1]] = roi
+        if result != 'neutral':
+            cv2.putText(rgba, result, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[result], 1)
+            cv2.rectangle(rgba, (x, y), (x+w, y+h), colors[result], 5)
 
-    return img
+    return rgba
